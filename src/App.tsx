dce1e5cd781ca1508226d2bc1e69a2b0a110fcd0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-25 23:05:00 -0500

    🚧 Create Station component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Entrance from "./components/entrance/Entrance";
+import Station from "./components/station/Station";
 
 function App() {
   return (
     <main className="grid h-screen grid-cols-4 bg-gray-300">
       <Entrance />
+      <Station />
     </main>
   );
 }
---
+++ src/components/station/Station.tsx
@@ -0,0 +1,16 @@
+function Station() {
+  return (
+    <div>
+      <h1>Estacion 1</h1>
+      <p>Tiempo de servicio promedio</p>
+      <select placeholder="Distribución">
+        <option value="1">Distribucion de Poisson</option>
+        <option value="2">Distribucion exponencial</option>
+        <option value="3">Distribucion uniforme</option>
+      </select>
+      <input type="number" placeholder="Lambda" />
+    </div>
+  );
+}
+
+export default Station;
