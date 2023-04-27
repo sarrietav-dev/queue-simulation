a7953559871e73cdd73d839caa9439ab2624d49f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-26 21:53:29 -0500

    🚧 Add the poisson distribution

---
+++ src/core/distributions/poisson.ts
@@ -0,0 +1,39 @@
+type Limit = { lower: number; upper: number };
+
+export class Poisson {
+    constructor(private mean: number) {
+        this.generateLimits();
+    }
+
+    private limits: Limit[] = [];
+
+    getVariable(x: number): number {
+        return this.limits.findIndex((value) => {
+            return x >= value.lower && x < value.upper;
+        });
+    }
+
+    private generateLimits() {
+        let previous: number = 0;
+        let current: number = 0;
+
+        while (previous < 0.999) {
+            const upper = previous + this.probability(current);
+            this.limits.push({ lower: previous, upper });
+            previous = upper;
+            current++;
+        }
+
+        this.limits[this.limits.length - 1].upper = 1;
+    }
+
+    private probability(x: number) {
+        return (
+            (Math.pow(this.mean, x) * Math.exp(-this.mean)) / this.factorial(x)
+        );
+    }
+
+    private factorial(x: number): number {
+        return x <= 1 ? 1 : x * this.factorial(x - 1);
+    }
+}
