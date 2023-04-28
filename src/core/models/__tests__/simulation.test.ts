b64adf8ab20da06fe5e1751afb01f5353e4f9cf2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-28 17:05:42 -0500

    🧪 Add failing tests for simulation

---
+++ src/core/models/__tests__/simulation.test.ts
@@ -0,0 +1,11 @@
+import { it } from "vitest";
+
+it("starts the simulation correctly");
+
+it("passes a client from one station to another");
+
+it("runs the simulation for a given time");
+
+it("runs the simulation until all clients are served");
+
+it("computes the longest queue length");
