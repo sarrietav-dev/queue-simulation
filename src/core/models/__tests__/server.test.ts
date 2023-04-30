18731a2fd7cc3f360686988f37e44165c5156527
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-30 01:38:13 -0500

    ✅ Fix tests

--- src/core/models/__tests__/server.test.ts
+++ src/core/models/__tests__/server.test.ts
@@ -2,13 +2,15 @@ import { expect, it, vi } from "vitest";
 import { Server } from "../Server";
 import { Random } from "../../../utils/random";
 import { Exponential } from "../../distributions/exponential";
+import { Client } from "../Client";
 
 it("should be busy when serving a person", () => {
     const dist = new Exponential(2);
     const random = new Random(1);
     const server = new Server(dist, random);
+    const client = new Client();
 
-    server.serve();
+    server.serve(client);
 
     expect(server.isBusy).toBe(true);
 });
@@ -17,9 +19,10 @@ it("should have a correct remaining time", () => {
     const dist = new Exponential(2);
     const random = new Random(1);
     const server = new Server(dist, random);
+    const client = new Client();
     vi.spyOn(random, "get").mockReturnValue(0.001964637);
 
-    server.serve();
+    server.serve(client);
 
     expect(server.isBusy).toBe(true);
     expect(server.timeRemaining).toBe(12);
