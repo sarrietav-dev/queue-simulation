25d1d5ca655a70346fae4706fc51089af417b314
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-05-01 16:58:56 -0500

    💄 Add entry rate form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 function App() {
-  return 
+    return (
+        <div className="container">
+            <h1>Simulador</h1>
+            <form action="">
+                <h2>Tasa de llegada</h2>
+                <div className="grid">
+                    <label htmlFor="entry_distribution">
+                        Distribución de llegada
+                        <select name="" id="entry_distribution">
+                            <option value="" selected>
+                                Selecciona una distribución
+                            </option>
+                            <option value="1">Uniforme</option>
+                            <option value="2">Exponencial</option>
+                            <option value="3">Poisson</option>
+                        </select>
+                    </label>
+                    <label htmlFor="entry_mean">
+                        Media
+                        <input type="text" name="" id="entry_mean" />
+                    </label>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default App;
