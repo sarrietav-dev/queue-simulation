a79b33300acbdec238b36e09e6302523d4d6e1e6
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-05-01 00:28:13 -0500

    ✅ Add more time duration tests

--- src/core/models/__tests__/simulation.test.ts
+++ src/core/models/__tests__/simulation.test.ts
@@ -1,4 +1,4 @@
-import { expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { SimulationBuilder } from "../Simulation";
 import { Station } from "../Station";
 import { ArrivalIterator } from "../ArrivalIterator";
@@ -6,7 +6,7 @@ import { Poisson } from "../../distributions/poisson";
 import { Random } from "../../../utils/random";
 import { Server } from "../Server";
 
-it("has 4 clients on the system", () => {
+it("serves every client on the simulation", () => {
     const arrivalIterator = new ArrivalIterator(
         new Poisson(0.333),
         new Random(2)
@@ -27,44 +27,87 @@ it("has 4 clients on the system", () => {
     expect(simulation.clientsServed).toBe(4);
 });
 
-it("has a total time of 8", () => {
+describe("Time duration tests", () => {
     const arrivalIterator = new ArrivalIterator(
         new Poisson(0.333),
         new Random(2)
     );
     vi.spyOn(arrivalIterator, "getArrivals").mockReturnValue([1, 1, 1, 1]);
 
-    const distribution = new Poisson(0.5);
-    vi.spyOn(distribution, "getVariable").mockReturnValue(2);
+    it("has a total time of 8", () => {
+        const distribution = new Poisson(0.5);
+        vi.spyOn(distribution, "getVariable").mockReturnValue(2);
 
-    const simulation = new SimulationBuilder()
-        .setStations([new Station(new Server(distribution, new Random(1)))])
-        .setArrivalIterator(arrivalIterator)
-        .setTimeStop(4)
-        .build();
+        const simulation = new SimulationBuilder()
+            .setStations([new Station(new Server(distribution, new Random(1)))])
+            .setArrivalIterator(arrivalIterator)
+            .setTimeStop(4)
+            .build();
 
-    simulation.run();
+        simulation.run();
 
-    expect(simulation.time).toBe(8);
-});
+        expect(simulation.time).toBe(8);
+    });
 
-it("has a total time of 14", () => {
-    const arrivalIterator = new ArrivalIterator(
-        new Poisson(0.333),
-        new Random(2)
-    );
-    vi.spyOn(arrivalIterator, "getArrivals").mockReturnValue([1, 2, 1, 3]);
+    it("has a total time of 5 with 2 servers", () => {
+        const distribution = new Poisson(0.5);
+        vi.spyOn(distribution, "getVariable").mockReturnValue(2);
 
-    const distribution = new Poisson(0.5);
-    vi.spyOn(distribution, "getVariable").mockReturnValue(2);
+        const simulation = new SimulationBuilder()
+            .setStations([
+                new Station(
+                    new Server(distribution, new Random(1)),
+                    new Server(distribution, new Random(1))
+                ),
+            ])
+            .setArrivalIterator(arrivalIterator)
+            .setTimeStop(4)
+            .build();
 
-    const simulation = new SimulationBuilder()
-        .setStations([new Station(new Server(distribution, new Random(1)))])
-        .setArrivalIterator(arrivalIterator)
-        .setTimeStop(4)
-        .build();
+        simulation.run();
 
-    simulation.run();
+        expect(simulation.time).toBe(5);
+    });
 
-    expect(simulation.time).toBe(14);
+    it("has a total time of 8 with 3 servers and serving time of 4", () => {
+        const distribution = new Poisson(0.5);
+        vi.spyOn(distribution, "getVariable").mockReturnValue(4);
+
+        const simulation = new SimulationBuilder()
+            .setStations([
+                new Station(
+                    new Server(distribution, new Random(1)),
+                    new Server(distribution, new Random(1)),
+                    new Server(distribution, new Random(1))
+                ),
+            ])
+            .setArrivalIterator(arrivalIterator)
+            .setTimeStop(4)
+            .build();
+
+        simulation.run();
+
+        expect(simulation.time).toBe(8);
+    });
+
+    it("has a total time of 14", () => {
+        const arrivalIterator = new ArrivalIterator(
+            new Poisson(0.333),
+            new Random(2)
+        );
+        vi.spyOn(arrivalIterator, "getArrivals").mockReturnValue([1, 2, 1, 3]);
+
+        const distribution = new Poisson(0.5);
+        vi.spyOn(distribution, "getVariable").mockReturnValue(2);
+
+        const simulation = new SimulationBuilder()
+            .setStations([new Station(new Server(distribution, new Random(1)))])
+            .setArrivalIterator(arrivalIterator)
+            .setTimeStop(4)
+            .build();
+
+        simulation.run();
+
+        expect(simulation.time).toBe(14);
+    });
 });
