b996e01d6556c7b8efa5e00ebe23d2293265a9db
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-05-03 00:35:48 -0500

    ➕ Add electron

--- src/core/distributions/exponential.ts
+++ src/core/distributions/exponential.ts
@@ -1,9 +1,9 @@
-import { Distribution } from "./Distribution";
+import type { Distribution } from './Distribution'
 
 export class Exponential implements Distribution {
   constructor(private lambda: number) {}
 
   getVariable(x: number): number {
-    return -this.lambda * Math.log(x);
+    return -this.lambda * Math.log(x)
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.IS_DEV !== 'true' ? './' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
