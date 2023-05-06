399e9ed386e65da7d1049c309fddf6ecbac7225a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-05-06 04:20:46 -0500

    🐛 Fix simulation not serving all the clients

--- src/core/models/Server.ts
+++ src/core/models/Server.ts
@@ -1,49 +1,49 @@
-import type { Random } from "../../utils/random";
-import type { Distribution } from "../distributions/Distribution";
-import type { Client } from "./Client";
+import type { Random } from '../../utils/random'
+import type { Distribution } from '../distributions/Distribution'
+import type { Client } from './Client'
 
 export class Server {
   constructor(private servingRate: Distribution, private random: Random) {}
 
-  private timeRemainingUntilFree: number = -1;
-  private clientBeingServed?: Client;
-  private _onClientServed: (client: Client) => void = () => {};
+  private timeRemainingUntilFree: number = -1
+  private clientBeingServed?: Client
+  private _onClientServed: (client: Client) => void = () => {}
 
   set onClientServed(onClientServed: (client: Client) => void) {
-    this._onClientServed = onClientServed;
+    this._onClientServed = onClientServed
   }
 
   get isBusy(): boolean {
-    return this.timeRemainingUntilFree > 0;
+    return this.timeRemainingUntilFree > 0
   }
 
   get timeRemaining(): number {
-    return this.timeRemainingUntilFree;
+    return this.timeRemainingUntilFree
   }
 
   serve(client: Client) {
-    this.clientBeingServed = client;
-    this.timeRemainingUntilFree = Math.round(
-      this.servingRate.getVariable(this.random.get())
-    );
+    this.clientBeingServed = client
+    while (this.timeRemainingUntilFree <= 0) {
+      this.timeRemainingUntilFree = Math.round(this.servingRate.getVariable(this.random.get()))
+    }
   }
 
   tick(): void {
     if (this.timeRemainingUntilFree > 0) {
-      this.timeRemainingUntilFree--;
+      this.timeRemainingUntilFree--
     }
   }
 
   notifyIfFinished(): void {
     if (this.timeRemainingUntilFree === 0) {
-      this.notifyStation();
+      this.notifyStation()
     }
   }
 
   notifyStation() {
     if (this.clientBeingServed) {
-      this._onClientServed(this.clientBeingServed);
-      this.clientBeingServed = undefined;
+      this._onClientServed(this.clientBeingServed)
+      this.clientBeingServed = undefined
     }
   }
 }
