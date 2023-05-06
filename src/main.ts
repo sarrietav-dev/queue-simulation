8c6a5a27ba4b1d7d00b5f3b82047766cd286cb10
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-05-05 19:47:42 -0500

    ✨ Integrate routing to the app

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@picocss/pico/css/pico.min.css'
-import { createPinia } from 'pinia';
+import { createPinia } from 'pinia'
+import { router } from './router'
 
-const pinia = createPinia();
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.use(router)
 app.mount('#app')
